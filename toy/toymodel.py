48900c8682d2bb0a66a4d05b0e1589a8bf34ddd4
nnolte <niklas.nolte@cern.ch>
2022-02-23 19:32:33 -0500

    about 13.6% good models, a factor 2-3 better than with lr_init = 5e-1

--- toy/toymodel.py
+++ toy/toymodel.py
@@ -23,16 +23,22 @@ def no_norm(x, **kwargs):
     return x
 
 
-def get_model(unconstrained=False, dev=None, L=1):
-    norm = no_norm if unconstrained else direct_norm
+def get_model(unconstrained=False, dev=None, L=1, project=False):
+    if unconstrained:
+      norm = unconstrained
+    else:
+      norm = project_norm if project else direct_norm
     activation = torch.nn.ReLU() if unconstrained else GroupSort(1)
-    layer_conf = (1, 32, 32, 32, 1)
+    layer_conf = (1,4,4,1)
     nlayers = len(layer_conf) - 1
 
     layers = []
     for i in range(nlayers):
-        kind_i = "two-inf" if i == 0 else "inf"
-        lin = torch.nn.Linear(layer_conf[i], layer_conf[i + 1], bias=True)#i != nlayers - 1)
+        kind_i = "inf"
+        #kind_i = "two-inf" if i == 0 else "inf"
+        lin = torch.nn.Linear(
+            layer_conf[i], layer_conf[i + 1], bias=True
+        )  # i != nlayers - 1)
         layers.append(
             norm(lin, kind=kind_i, always_norm=False, alpha=L ** (1 / nlayers))
         )
--- toy/wasserstein-toy.py
+++ toy/wasserstein-toy.py
@@ -1,40 +1,35 @@
-import os
-
-os.environ["CUBLAS_WORKSPACE_CONFIG"] = ":4096:8"
-
 import torch
 import numpy as np
 import ot
 from tqdm import tqdm
 
 import matplotlib.pyplot as plt
+
 plt.rcParams.update({"font.size": 12})
 
 
 from toymodel import get_model
 
-torch.use_deterministic_algorithms(True)
-torch.backends.cuda.matmul.allow_tf32 = False
-
-torch.random.manual_seed(3)
-
-plot=True
+plot = False
+plotfreq = 1000
+save = False#not plot
 
 unconstrained = False
 L = 1
-EPOCHS = 15000
+EPOCHS = 5000
 lr_init = 5e-2
-lr_final = 2e-4
+lr_final = 5e-3
 gamma = (lr_final / lr_init) ** (1 / EPOCHS)
-dev = torch.device('cpu')
+dev = torch.device("cpu")
 
-model = get_model(unconstrained=unconstrained, dev=dev, L=L)
+nps = 5
 
-nps = 11
-
-ps = np.linspace(0,10,nps)#.reshape(-1, 1)
+ps = np.linspace(0, nps - 1, nps)
 qs = ps[np.round(ps) % 2 == 0].reshape(-1, 1)
 ps = ps[np.round(ps) % 2 == 1].reshape(-1, 1)
+# add some more data point to the immediate left / right does not help
+# ps = np.concatenate([ps+.1, ps - .1, ps])
+# qs = np.concatenate([qs+.1, qs - .1, qs])
 pEs = np.ones(len(ps))
 pEs = pEs / pEs.sum()
 np.savez(f"ps.npz", ps=ps, pEs=pEs)
@@ -49,75 +44,90 @@ qdists = torch.tensor(M.min(axis=0).reshape(-1, 1)).to(dev)
 distance = ot.emd2(pEs, qEs, M)
 Mt = torch.tensor(M).to(dev)
 
-
 pst = torch.from_numpy(ps).float().to(dev)
 qst = torch.from_numpy(qs).float().to(dev)
 pEst = torch.from_numpy(pEs).float().to(dev).view(-1, 1)
 qEst = torch.from_numpy(qEs).float().to(dev).view(-1, 1)
 
-max_emd = 0
-optim = torch.optim.Adam(model.parameters(), lr=lr_init)
-scheduler = torch.optim.lr_scheduler.ExponentialLR(optim, gamma=gamma)
 
-bar = tqdm(range(EPOCHS))
+def evaluate(seed):
+    torch.random.manual_seed(seed)
 
-minx = min(qs.min(), ps.min())
-maxx = max(qs.max(), ps.max())
+    model = get_model(unconstrained=unconstrained, dev=dev, L=L, project=False)
 
-if plot:
+    max_emd = 0
+    optim = torch.optim.Adam(model.parameters(), lr=lr_init)
+    scheduler = torch.optim.lr_scheduler.ExponentialLR(optim, gamma=gamma)
 
-  fig, ax = plt.subplots()
+    if plot:
+        minx = min(qs.min(), ps.min())
+        maxx = max(qs.max(), ps.max())
 
-  title = ax.text(0.05, 0.8, "", transform=ax.transAxes)
+        _, ax = plt.subplots()
 
-  nbinsPP = 2
-  nbins = (len(ps)+ len(qs))*nbinsPP - nbinsPP + 1
-  diff_from_0 = ((len(ps) + len(qs)) * nbinsPP * 4) ** -1
+        title = ax.text(0.05, 0.8, "", transform=ax.transAxes)
 
-  X = np.linspace(minx, maxx, nbins).reshape(-1, 1)
-  Xt = torch.from_numpy(X).float().to(dev)
+        nbinsPP = 2
+        nbins = (len(ps) + len(qs)) * nbinsPP - nbinsPP + 1
 
-  ax.scatter(qs[:, 0], qs[:, 0] * 0, s=qEs * 1000, c="royalblue")
-  ax.scatter(ps[:, 0], ps[:, 0] * 0, s=pEs * 1000, c="crimson")
+        X = np.linspace(minx, maxx, nbins).reshape(-1, 1)
+        Xt = torch.from_numpy(X).float().to(dev)
 
-  maxys = [-10, 10]#[-.25 - diff_from_0, .25 - diff_from_0]
+        ax.scatter(qs[:, 0], qs[:, 0] * 0, s=qEs * 1000, c="royalblue")
+        ax.scatter(ps[:, 0], ps[:, 0] * 0, s=pEs * 1000, c="crimson")
 
-  ax.hlines(maxys, minx, maxx, linestyles="dashed")
-  with torch.no_grad():
-    h = ax.plot(X, np.random.choice(maxys, len(X)), c="black")
+        maxys = [-10, 10]
 
-bar = tqdm(range(EPOCHS))
-for i in bar:
-    with torch.no_grad():
-      up = torch.randn_like(pst).to(dev) / 100
-      uq = torch.randn_like(qst).to(dev) / 100
-    mp = model(pst + up)
-    mq = model(qst + uq)
-    E0 = (mp * pEst).sum()
-    E1 = (mq * qEst).sum()
-    if unconstrained:
-      factor = ((mp - mq.view(-1)).abs() / Mt).max()
-      emd = (E1 - E0) / factor
-    else:
-      emd = E1 - E0
-    with torch.no_grad():
-      if i % 10 == 0:
-        full_emd = (model(qst) * qEst).sum() - (model(pst) * pEst).sum()
-        if full_emd > max_emd:
-          delta = (max_emd - distance) / distance * 100
-          max_emd = full_emd
-          if plot:
-            Yt = model(Xt)
-            Yt = Yt - Yt.mean()
-            h[0].set_ydata(Yt.numpy())
-            plt.pause(1e-10)
-            title.set_text(f"{max_emd:.3f} vs {distance:.3f} - Delta: {delta:.4f}% @ {i}")
-          bar.set_description(f"{max_emd:.3f} vs {distance:.3f} - Delta: {delta:.4f}% @ {i}")
-    loss = -emd
-    loss.backward()
-    optim.step()
-    scheduler.step()
-    optim.zero_grad()
+        ax.hlines(maxys, minx, maxx, linestyles="dashed")
+        with torch.no_grad():
+            h = ax.plot(X, np.random.choice(maxys, len(X)), c="black")
 
-torch.save(model.state_dict(), f"model.pt")
-plt.show()
+    bar = tqdm(range(EPOCHS))
+    for i in bar:
+        mp = model(pst)
+        mq = model(qst)
+        E0 = (mp * pEst).sum()
+        E1 = (mq * qEst).sum()
+        if unconstrained:
+            factor = ((mp - mq.view(-1)).abs() / Mt).max()
+            emd = (E1 - E0) / factor
+        else:
+            emd = E1 - E0
+        with torch.no_grad():
+            delta = (max_emd - distance) / distance * 100
+            if i % 100 == 0:
+                if emd > max_emd:
+                    max_emd = emd
+                    bar.set_description(
+                        f"{emd:.3f} vs {distance:.3f} - Delta: {delta:.4f}% @ {i}"
+                    )
+            if plot and i % plotfreq == 0:
+                Yt = model(Xt)
+                Yt = Yt - Yt.mean()
+                h[0].set_ydata(Yt.numpy())
+                plt.pause(1e-10)
+                title.set_text(
+                    f"{max_emd:.3f} vs {distance:.3f} - Delta: {delta:.4f}% @ {i}"
+                )
+        loss = -emd
+        loss.backward()
+        optim.step()
+        scheduler.step()
+        optim.zero_grad()
+    
+    if save:
+      metric = "good" if delta > -1 else "bad"
+      torch.save(model.state_dict(), f"models/{metric}/model{seed}.pt")
+
+    if plot:
+      plt.show()
+    
+    return delta
+
+if __name__ == __name__:
+    import multiprocessing as mp
+    #evaluate(3)
+    results = list(mp.Pool(110).map(evaluate, range(1000)))
+    print((np.array(results) > -5).mean())
+    breakpoint()
+    
